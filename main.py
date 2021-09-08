7f5daf1c084973a41e72c82166679704a2c762a1
pechenzz <punctuwuation@gmail.com>
2021-09-08 22:43:51 +0300

    weeeee

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+from hooman import Hooman
 import pygame
+import pygame.freetype
 import settings
 
-def main():
-    pygame.init()
-
-    pygame.display.set_caption("picolor")
-    screen = pygame.display.set_mode((640,480))
-
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-    
-
-    screen.fill((255, 255, 255))
+window_width, window_height = 640, 480
+hapi = Hooman(window_width, window_height)
+GAME_FONT = pygame.freetype.Font("inkythinpixels.ttf", 24)
 
 
-if __name__=="__main__":
-    main()
+slider_options = {"value_range": [0, 255]}
+
+r_slider = hapi.slider(50, 300, 400, 10, slider_options)
+g_slider = hapi.slider(50, 340, 400, 10, slider_options)
+b_slider = hapi.slider(50, 380, 400, 10, slider_options)
+
+while hapi.is_running:
+    if settings.getsettings()["darkmode"] == 1:
+            hapi.background((11, 11, 11))
+    elif settings.getsettings()["darkmode"] == 0:
+        hapi.background((255, 255, 255))
+    else:
+        hapi.background((255, 255, 255))
+
+    r_slider.update()
+    g_slider.update()
+    b_slider.update()
+
+    """hapi.fill(hapi.color["black"])
+    r_text = "r:{}".format(round(r_slider.value()))
+    hapi.text(r_text, 50, 280)
+    g_text = "g:{}".format(round(g_slider.value()))
+    hapi.text(g_text, 50, 310)
+    b_text = "b:{}".format(round(b_slider.value()))
+    hapi.text(b_text, 50, 340)"""
+
+    text_surface, rect = GAME_FONT.render(f"R: {round(r_slider.value())}", fgcolor = (255, 255, 255), size = 18)
+    hapi.screen.blit(text_surface, ((400-text_surface.get_width())/2+50, 280))
+    text_surface1, rect1 = GAME_FONT.render(f"G: {round(g_slider.value())}", fgcolor = (255, 255, 255), size = 18)
+    hapi.screen.blit(text_surface1, ((400-text_surface1.get_width())/2+50, 320))
+    text_surface2, rect2 = GAME_FONT.render(f"B: {round(b_slider.value())}", fgcolor = (255, 255, 255), size = 18)
+    hapi.screen.blit(text_surface2, ((400-text_surface2.get_width())/2+50, 360))
+
+    hapi.flip_display()
+    hapi.event_loop()
+
+pygame.quit()
