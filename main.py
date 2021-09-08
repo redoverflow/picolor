6f9c8c282500c4b11b3d37fa19416875241e4827
pechenzz <punctuwuation@gmail.com>
2021-09-08 23:00:23 +0300

    funny

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ GAME_FONT = pygame.freetype.Font("inkythinpixels.ttf", 24)
 
 slider_options = {"value_range": [0, 255]}
 
-r_slider = hapi.slider(50, 300, 400, 10, slider_options)
-g_slider = hapi.slider(50, 340, 400, 10, slider_options)
-b_slider = hapi.slider(50, 380, 400, 10, slider_options)
+r_slider = hapi.slider((window_width-400)/2, 300, 400, 10, slider_options)
+g_slider = hapi.slider((window_width-400)/2, 340, 400, 10, slider_options)
+b_slider = hapi.slider((window_width-400)/2, 380, 400, 10, slider_options)
 
 while hapi.is_running:
     if settings.getsettings()["darkmode"] == 1:
@@ -26,20 +26,15 @@ while hapi.is_running:
     g_slider.update()
     b_slider.update()
 
-    """hapi.fill(hapi.color["black"])
-    r_text = "r:{}".format(round(r_slider.value()))
-    hapi.text(r_text, 50, 280)
-    g_text = "g:{}".format(round(g_slider.value()))
-    hapi.text(g_text, 50, 310)
-    b_text = "b:{}".format(round(b_slider.value()))
-    hapi.text(b_text, 50, 340)"""
-
     text_surface, rect = GAME_FONT.render(f"R: {round(r_slider.value())}", fgcolor = (255, 255, 255), size = 18)
-    hapi.screen.blit(text_surface, ((400-text_surface.get_width())/2+50, 280))
+    hapi.screen.blit(text_surface, (((window_width-text_surface.get_width())/2, 280)))
     text_surface1, rect1 = GAME_FONT.render(f"G: {round(g_slider.value())}", fgcolor = (255, 255, 255), size = 18)
-    hapi.screen.blit(text_surface1, ((400-text_surface1.get_width())/2+50, 320))
+    hapi.screen.blit(text_surface1, (((window_width-text_surface1.get_width())/2, 320)))
     text_surface2, rect2 = GAME_FONT.render(f"B: {round(b_slider.value())}", fgcolor = (255, 255, 255), size = 18)
-    hapi.screen.blit(text_surface2, ((400-text_surface2.get_width())/2+50, 360))
+    hapi.screen.blit(text_surface2, (((window_width-text_surface2.get_width())/2, 360)))
+
+    hapi.fill((r_slider.value(), g_slider.value(), b_slider.value()))
+    hapi.rect((window_width-250)/2, 0, 250, 250)
 
     hapi.flip_display()
     hapi.event_loop()
